bbf27ac2b7350f0ae7de9d15f89fdacb48550ae7
mahedi5061 <mahedi5061@gmail.com>
2022-02-14 12:30:01 +0600

    Hexadecimal color generate

--- HexadecimalCodeGenerate/app.js
+++ HexadecimalCodeGenerate/app.js
@@ -8,19 +8,21 @@ window.onload = () =>{
 function main() {
     const root=document.getElementById("root");
     const btn=document.getElementById("change-btn");
+    const output=document.getElementById("output")
 
     btn.addEventListener('click',function(){
-        const bgColor=colorGeneratorRGB();
+        const bgColor=colorHexGenerator();
         root.style.backgroundColor = bgColor;
+        output.value=bgColor;
     })
 }
 
 //step-3 color generate.
 
-function colorGeneratorRGB() {
+function colorHexGenerator() {
     const red=Math.floor(Math.random() * 255);
     const green=Math.floor(Math.random() * 255);
     const blue=Math.floor(Math.random() * 255);
 
-    return `rgb(${red},${green},${blue})`;
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
